6c618d1426be7b1f9ecc12c7fdd90d1d900a1981
ForbesR86 <89602329+ForbesR86@users.noreply.github.com>
2021-10-15 12:11:54 -0400

    Update VideoRecommended.jsx

--- src/components/VideoRecommended/VideoRecommended.jsx
+++ src/components/VideoRecommended/VideoRecommended.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import Video from '../Video/Video';
 
 const VideoRecommended = ({videos , videoSelectHandler}) => {
-    
-    return (
-      <div className="card-group">
-      {videos.map((video) => {
-        return (
-          <Video video={video} videoSelectHandler={videoSelectHandler} key={video.id.videoId}/> 
-        );
-      })}
-      </div>
-    )
+  if (!videos) {
+    return <div>No Video</div>;
+  }
+
+  return (
+    <div className="card-group">
+    {videos.map((video) => {
+      return (
+        <Video video={video} videoSelectHandler={videoSelectHandler} key={video.id.videoId}/> 
+      );
+    })}
+    </div>
+  )
 };
 
 export default VideoRecommended;
