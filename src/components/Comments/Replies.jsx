700c57095635e76e87b82796c12be5dc9ed58cb4
ForbesR86 <89602329+ForbesR86@users.noreply.github.com>
2021-10-19 08:15:59 -0400

    replies cleanup
    replies cleanup

--- src/components/Comments/Replies.jsx
+++ src/components/Comments/Replies.jsx
@@ -2,17 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 //'http://127.0.0.1:8000/replies/' + commentID + '/'
 const Replies = ({commentID}) => {
-// Get replies
-// let repliesList = [];
-// axios.get('http://127.0.0.1:8000/replies/' + commentID + '/')
-// .then(response => {
-//      repliesList = response.data
-//     })
-// .catch(function(error) {
-//    console.log(error);
-// })
-// console.log(commentID);
-// console.log(repliesList)
 
 let [responseData, setResponseData] = React.useState([]);
   const fetchData = React.useCallback(() => {
@@ -33,9 +22,8 @@ let [responseData, setResponseData] = React.useState([]);
 //let childComments = () => repliesList.filter(c => c.commentid === commentID)
 
 if (responseData) {
-    console.log(commentID + 'no child')
+    console.log(commentID + ' no child')
 }
-console.log(responseData)
     return (
             <><ul>    
                 {responseData.map(Reply => {
@@ -55,8 +43,3 @@ console.log(responseData)
 }
 
 export default Replies;
-
-<ul>
-      <li>Black tea</li>
-      <li>Green tea</li>
-    </ul>
